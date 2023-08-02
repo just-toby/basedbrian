19b8104bc847733d906b526c520063db09e6bef9
Lotachi <lotaoko2@gmail.com>
2023-08-02 22:09:01 +0100

    added links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,15 +16,25 @@ function Hero() {
         <img src={herotextt} alt="" className='w-80 mb-10     lg:w-[700px]' />
        
        <div className="buttons flex flex-col items-center">
+        <a href="#">
         <img src={mint} alt="" className='w-40 mb-5     lg:w-64 lg:mb-10' />
-        <img src={gal} alt="" className='w-44 mb-5     lg:w-72 lg:mb-10' />
+        </a>
+<a href="#">
+<img src={gal} alt="" className='w-44 mb-5     lg:w-72 lg:mb-10' />
+</a>
 
        </div>
 
        <div className="social buttons flex items-center mb-10">
-        <img src={discord} alt="" className='w-9 mb-5 mx-3      lg:w-14' />
+       <a href="">
+       <img src={discord} alt="" className='w-9 mb-5 mx-3      lg:w-14' />
+       </a>
+        <a href="">
         <img src={telegram} alt="" className='w-9 mb-5 mx-3      lg:w-14' />
+        </a>
+        <a href="">
         <img src={twitter} alt="" className='w-9 mb-5 mx-3      lg:w-14' />
+        </a>
 
        </div>
        
--- src/components/mini/Mint.js
+++ src/components/mini/Mint.js
@@ -20,7 +20,9 @@ function Mint() {
        <div className="mintsection flex flex-col justify-center items-center mb-10">
 
 <div className="mintbutton flex justify-center  ">
+    <a href="" className="button">
     <img src={mintnow} alt="" className='w-44 mb-5     lg:w-64 lg:mb-2' />
+    </a>
     <input className='  w-[52px] h-[52px] rounded-lg ml-2 lg:h-[77px] lg:w-[77px] text-center lg:text-xl' type="text"  placeholder='1'/>
 </div>
     <input className=' w-[234px] h-[52px] rounded-lg ml-2 lg:h-[77px] lg:w-[343px] text-center lg:text-xl !m-0 p-0' type="text"
