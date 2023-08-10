7693d7e8c56de0093b096f9d4e5f908338e6dbbb
rollodex <michael.glenn837@gmail.com>
2023-08-10 00:09:09 -0400

    popups and terms

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import Hero from "./components/Hero";
 import Hidden from "./components/Hidden";
 import World from "./components/World";
+import TermsModal from './components/TermsModal';
+import BaseChainModal from './components/BaseChainModal';
+import {ethers } from 'ethers';
+
 import './App.css'
 
 
 function App() {
+  const [getProvider, setProvider] = useState(null);
+  const [getSigner, setSigner] = useState(null);
+  const [isBased, setIsBased] = useState(false);
+  
+
   useEffect(() => {
     document.title = "Based Brian";
+  
+
+
+    const initEthers = async () => {
+
+      // Prompt user to connect their MetaMask account to your site
+     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+     
+     // If no account is connected, stop here
+     if (accounts.length === 0) {
+       console.log('Please connect your MetaMask account.');
+       return;
+     }
+
+     // Get network id using window.ethereum
+     const networkId = await window.ethereum.request({ method: 'net_version' });
+     
+const chainCheck = () => {
+     if (networkId != 8453) {
+       //alert("You aren't based if you aren't on the BASE network. Please switch to BASE network and try again.");
+       setIsBased(false);
+     } else {
+       setIsBased(true);
+     }
+
+   }
+   chainCheck()
+     //Run the same logic every time the network changes
+     window.ethereum.on('chainChanged', chainCheck);
+
+     // Define a mapping from network ID to RPC URL
+     const rpcUrls = {
+       
+       8453: 'https://mainnet.base.org'                       // Custom network
+       // Add more networks if needed
+     };
+
+     // Get the correct RPC URL for the current network
+     const rpcUrl = rpcUrls[networkId];
+
+     const provider = new ethers.BrowserProvider(window.ethereum)
+     const signer = await provider.getSigner();
+     
+     setProvider(provider);
+     setSigner(signer);
+   };
+
+    if (window.ethereum) {
+      initEthers();
+    }
+
   }, []);
   return (
     <div>
       <Hero/>
-      <World/>
+      <World passProvider={getProvider} passSigner={getSigner} passBased={isBased}/>
+      <BaseChainModal isOpen={!isBased}/>
+      <TermsModal/>
 <Hidden/> 
 
 
--- src/components/mini/Mint.js
+++ src/components/mini/Mint.js
@@ -1,65 +1,25 @@
 import { useState, useEffect } from 'react';
 import hero from "../../assets/hero2.svg"
 import mintnow from "../../assets/mintnow.svg"
+import MintModal from './MintModal';
 
 
 import { FixedNumber, ethers } from 'ethers';
 
-function Mint() {
-     const [provider, setProvider] = useState(null);
-     const [signer, setSigner] = useState(null);
+function Mint({mainProvider,mainSigner, weBased}) {
+     const [provider, setProvider] = useState(mainProvider);
+     const [signer, setSigner] = useState(mainSigner);
      const [mintQty, setMintQty] = useState(1);
-     const [isBased, setIsBased] = useState(false);
+     const [isBased, setIsBased] = useState(weBased);
+     const [didMint, setDidMint] = useState(false);
 
   // Initialize ethers.js with a custom RPC URL
   useEffect(() => {
-    const initEthers = async () => {
+   
 
-       // Prompt user to connect their MetaMask account to your site
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      
-      // If no account is connected, stop here
-      if (accounts.length === 0) {
-        console.log('Please connect your MetaMask account.');
-        return;
-      }
-
-      // Get network id using window.ethereum
-      const networkId = await window.ethereum.request({ method: 'net_version' });
-      
-const chainCheck = () => {
-      if (networkId != 8453) {
-        alert("You aren't based if you aren't on the BASE network. Please switch to BASE network and try again.");
-        setIsBased(false);
-      } else {
-        setIsBased(true);
-      }
-
-    }
-    chainCheck()
-      //Run the same logic every time the network changes
-      //window.ethereum.on('chainChanged', chainCheck());
-
-      // Define a mapping from network ID to RPC URL
-      const rpcUrls = {
-        
-        8453: 'https://mainnet.base.org'                       // Custom network
-        // Add more networks if needed
-      };
-
-      // Get the correct RPC URL for the current network
-      const rpcUrl = rpcUrls[networkId];
-
-      const provider = new ethers.BrowserProvider(window.ethereum)
-      const signer = await provider.getSigner();
-      
-      setProvider(provider);
-      setSigner(signer);
-    };
-
-    if (window.ethereum) {
-      initEthers();
-    }
+   // if (window.ethereum) {
+   //   initEthers();
+   // }
   }, []);
 
   // Function to mint a new token
@@ -727,6 +687,7 @@ const chainCheck = () => {
 
    // Get the mint price as a big number hardcoded to 0.069 ETH and multiply by the quantity
    const mintPrice = ethers.parseEther('0.069')  * ethers.toBigInt(mintQty.toString())
+   setDidMint(false);
    
   //alert(ethers.formatEther(mintPrice).toString());
 
@@ -734,49 +695,56 @@ const chainCheck = () => {
         //Call the mintToken function of the contract
         const tx = await contract.mint(parseInt(mintQty), { value: mintPrice });
 
-        // Wait for the transaction to be mined
+            // Wait for the transaction to be mined
 
-        const receipt = await tx.wait();
-        console.log(receipt);
-        alert("Thank you for minting! You are now based.")
-     } catch (error) {
-        console.log(error);
-        //See if it is a revert due to insufficent ether and alert the user:
-        if (error.message.includes("missing revert data")) {
-           alert("Insufficient Ether to mint this token");
-        }
-
-     }
+            const receipt = await tx.wait();
+            console.log(receipt);
+            //alert("Thank you for minting! You are now based.")
+            setDidMint(true);
+         } catch (error) {
+            console.log(error);
+            //See if it is a revert due to insufficent ether and alert the user:
+            if (error.message.includes("missing revert data")) {
+               alert("Insufficient Ether to mint this token");
+            }
+            setDidMint(false);
     
-  };
-
-  return (
-    <div className="lg:pl-60 mint bg-grdeen-900 w-full lg:w-2/4 flex flex-col items-center  ">
-    <img src={hero} alt="" className='w-60     lg:w-52' />
-
-
-    <h3 className='text-xl lg:text-2xl self-center justify-self-center align mt-5 text-center'>
-    3333 Baldies
-       </h3>
-    <h3 className='text-xl lg:text-2xl self-center justify-self-center align mt-5 mb-5 text-center'>
-    Mint price : 0.1 ETH
-       </h3>
-
-       
-
-       <div className="mintsection flex flex-col justify-center items-center mb-10">
-
-<button onClick={mintToken} className="button" disabled={!isBased}>
-    <img src={mintnow} alt="" className='w-44 mb-5 lg:w-64 lg:mb-2' />
-</button>
-    <input className=' w-[234px] h-[52px] rounded-lg ml-2 lg:h-[77px] lg:w-[343px] text-center lg:text-xl !m-0 p-0' type="text"
-    placeholder='Enter Quantity' value={mintQty} onChange={(e) => setMintQty(e.target.value)}
-    />
-
-
-       </div>
-
-    </div>
+         }
+        
+      };
+    
+      return (
+          
+        <div className="lg:pl-60 mint bg-grdeen-900 w-full lg:w-2/4 flex flex-col items-center  ">
+                   {didMint && <MintModal didMint={didMint} />}
+    
+    
+        <img src={hero} alt="" className='w-60     lg:w-52' />
+    
+    
+        <h3 className='text-xl lg:text-2xl self-center justify-self-center align mt-5 text-center'>
+        3333 Baldies
+           </h3>
+        <h3 className='text-xl lg:text-2xl self-center justify-self-center align mt-5 mb-5 text-center'>
+        Mint price : 0.069 ETH
+           </h3>
+    
+           
+    
+           <div className="mintsection flex flex-col justify-center items-center mb-10">
+    
+    <button onClick={mintToken} className="button" disabled={!isBased}>
+        <img src={mintnow} alt="" className='w-44 mb-5 lg:w-64 lg:mb-2' />
+    </button>
+        <input className=' w-[234px] h-[52px] rounded-lg ml-2 lg:h-[77px] lg:w-[343px] text-center lg:text-xl !m-0 p-0' type="text"
+        placeholder='Enter Quantity' value={mintQty} onChange={(e) => setMintQty(e.target.value)}
+        />
+    
+    
+           </div>
+    
+           
+</div>        
   )
 }
 
