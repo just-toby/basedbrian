279d2ee819990421efec6b3f5a9016cc5b42d13c
just-toby <ejd4@protonmail.com>
2023-08-13 23:30:29 -0700

    RainbowKit+Viem update, lints

--- src/App.js
+++ src/App.js
@@ -1,86 +1,53 @@
-import React, { useEffect,useState } from 'react';
+import "@rainbow-me/rainbowkit/styles.css";
+import "./App.css";
+
+import React from "react";
 import Hero from "./components/Hero";
 import Hidden from "./components/Hidden";
 import World from "./components/World";
-import TermsModal from './components/TermsModal';
-import BaseChainModal from './components/BaseChainModal';
-import {ethers } from 'ethers';
+import TermsModal from "./components/TermsModal";
+import BaseChainModal from "./components/BaseChainModal";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-import './App.css'
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { base } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
+const { chains, publicClient } = configureChains(
+  [base],
+  [
+    alchemyProvider({ apiKey: "tgLbjQVoz3UhQnSQ1o4BBLwFDNsTqL_0" }),
+    publicProvider(),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: "Based Brians",
+  projectId: "d4572d71ba87eda8d21480e6850549d3",
+  chains,
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 function App() {
-  const [getProvider, setProvider] = useState(null);
-  const [getSigner, setSigner] = useState(null);
-  const [isBased, setIsBased] = useState(false);
-  
-
-  useEffect(() => {
-    document.title = "Based Brian";
-  
-
-
-    const initEthers = async () => {
-
-      // Prompt user to connect their MetaMask account to your site
-     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-     
-     // If no account is connected, stop here
-     if (accounts.length === 0) {
-       console.log('Please connect your MetaMask account.');
-       return;
-     }
-
-     // Get network id using window.ethereum
-     const networkId = await window.ethereum.request({ method: 'net_version' });
-     
-const chainCheck = () => {
-     if (networkId != 8453) {
-       //alert("You aren't based if you aren't on the BASE network. Please switch to BASE network and try again.");
-       setIsBased(false);
-     } else {
-       setIsBased(true);
-     }
-
-   }
-   chainCheck()
-     //Run the same logic every time the network changes
-     window.ethereum.on('chainChanged', chainCheck);
-
-     // Define a mapping from network ID to RPC URL
-     const rpcUrls = {
-       
-       8453: 'https://mainnet.base.org'                       // Custom network
-       // Add more networks if needed
-     };
-
-     // Get the correct RPC URL for the current network
-     const rpcUrl = rpcUrls[networkId];
-
-     const provider = new ethers.BrowserProvider(window.ethereum)
-     const signer = await provider.getSigner();
-     
-     setProvider(provider);
-     setSigner(signer);
-   };
-
-    if (window.ethereum) {
-      initEthers();
-    }
-
-  }, []);
   return (
-    <div>
-      <Hero/>
-      <World passProvider={getProvider} passSigner={getSigner} passBased={isBased}/>
-      <BaseChainModal isOpen={!isBased}/>
-      <TermsModal/>
-<Hidden/> 
-
-
-
-    </div>
-  )
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <div style={{ position: "absolute", top: 24, right: 24 }}>
+          <ConnectButton />
+        </div>
+        <Hero />
+        <World />
+        <BaseChainModal />
+        <TermsModal />
+        <Hidden />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default App
+export default App;
--- src/components/BaseChainModal.js
+++ src/components/BaseChainModal.js
@@ -1,40 +1,54 @@
-﻿import { useState } from "react";
+import { useNetwork, useSwitchNetwork } from "wagmi";
 
-function BaseChainModal({isOpen}) {
-    const [reminder,setReminder] = useState(isOpen);
-    if (!reminder) {
-        return null;
-    }
+function BaseChainModal() {
+  const { chain } = useNetwork();
+  // todo: handle failures etc
+  const { switchNetwork } = useSwitchNetwork({
+    chainId: 8453,
+    onError() {},
+    onMutate() {},
+    onSettled() {},
+    onSuccess() {},
+  });
 
-    return (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center">
-                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+  if (!chain || chain?.id === 8453) {
+    return null;
+  }
 
-                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all my-8 align-middle max-w-lg w-full">
-                    <div className="px-4 py-5 sm:px-6">
-                        <h3 className="text-lg leading-6 font-medium text-gray-900">BASED Alert</h3>
-                    </div>
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-                    <div className="px-6 py-4">
-                        <div className="text-sm text-gray-600">
-                            <p>You're not truly BASED unless you're on the BASE chain. Make the switch to get BASED.</p>
-                        </div>
-                    </div>
+        <div className="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all my-8 align-middle max-w-lg w-full">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              BASED Alert
+            </h3>
+          </div>
 
-                    <div className="px-6 py-4 bg-gray-50">
-                        <button
-                            type="button"
-                            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                            onClick={() => setReminder(false)}
-                        >
-                            Understood
-                        </button>
-                    </div>
-                </div>
+          <div className="px-6 py-4">
+            <div className="text-sm text-gray-600">
+              <p>
+                You're not truly BASED unless you're on the BASE chain. Make the
+                switch to get BASED.
+              </p>
             </div>
+          </div>
+
+          <div className="px-6 py-4 bg-gray-50">
+            <button
+              type="button"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+              onClick={() => switchNetwork(8453)}
+            >
+              Understood
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default BaseChainModal;
